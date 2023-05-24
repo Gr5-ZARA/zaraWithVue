37569e64f5bf959192e877e51333cccc709e913d
Islem-Az <isleem.azouuz@gmail.com>
2023-05-24 16:17:52 +0200

    commit to pull

--- zara-grp5/src/views/Help.vue
+++ zara-grp5/src/views/Help.vue
@@ -1,17 +1,27 @@
 <template>
     <div>
-      <div class="help">
+      <div class="help questions">
         <h4 class="head">HOW CAN WE HELP YOU?</h4>
         <input
           type="text"
           v-model="searchText"
           class="searchbar"
-          placeholder="look for"
+          placeholder="Look for"
           @input="handleSearchChange"
         />
+        <div>
+          <h5>FREQUENTLY ASKED QUESTIONS</h5>
+          <ul>
+            <li>ITEMS AVAILABILITY</li>
+            <li>HOW TO RETURN</li>
+            <li>ORDER STATUS</li>
+            <li>REFUNDS</li>
+          </ul>
+        </div>
       </div>
     </div>
   </template>
+
   
   <script lang="ts">
   import { defineComponent, ref } from 'vue';
@@ -36,15 +46,12 @@
   <style scoped>
   .help {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     background-image: url('https://static.zara.net/photos///contents/mkt/spots/ss23-help-customer/subhome-xmedia-10-north//w/1839/IMAGE-landscape-web-27470945-8c31-433a-9eaf-392114b6a91d-default_0.jpg?ts=1682518360676');
     background-repeat: no-repeat;
     min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
     margin-top: 10px;
   }
   
@@ -63,5 +70,24 @@
     margin-left: -650px;
     background: none;
   }
-  </style>
   
+  .questions {
+    margin-top: 2em;
+    text-align: center;
+  }
+  
+  .questions h5 {
+    font-size: 1.2em;
+    margin-bottom: 0.5em;
+  }
+  
+  .questions ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  }
+  
+  .questions li {
+    margin-bottom: 0.3em;
+  }
+  </style>
