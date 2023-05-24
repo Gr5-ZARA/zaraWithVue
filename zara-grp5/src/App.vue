d3ae0245782e3e2699aa2c919671b30c33e3c588
Islem-Az <isleem.azouuz@gmail.com>
2023-05-24 15:10:30 +0200

    commit to pull

--- zara-grp5/src/App.vue
+++ zara-grp5/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './views/Navbar.vue'
+import type HomeViewVue from './views/HomeView.vue';
 
 </script>
 
 <template>
   <header>
     <Navbar/>
+    <HomeViewVue />
    
   </header>
 
--- zara-grp5/src/router/index.ts
+++ zara-grp5/src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from "../views/HomeView.vue"
 import Login from '@/views/Login.vue'
 import Signup from '../views/Signup.vue'
 import Search from '../views/Search.vue'
+import Help from '../views/Help.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path:'/search',
       name:'Search',
       component:Search
+    },
+    {
+      path: '/help',
+      name: 'Help',
+      component: Help
     }
   ]
 })
