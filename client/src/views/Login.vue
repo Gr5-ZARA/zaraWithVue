720ce10e303c701a7c3c8ac18a86ee6a9c67cfd0
Islem-Az <isleem.azouuz@gmail.com>
2023-05-25 11:51:33 +0200

    help page restyled , menu icon on the navbar fixed

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,145 +1,153 @@
-  <template>
-    <div>
-      <div class="container-login" style="margin-top: 2vh;">
-        <div class="Left-login-form" style="margin-top: 40vh;">
-          <h3 class="login-heading">LOG IN TO YOUR ACCOUNT</h3>
-          <div class="form-input-label">
-            <input
-              class="form-input-label"
-              type="text"
-              id="fname"
-              name="email"
-              placeholder="E-MAIL"
-              v-model="useremail"
-            />
-            <input
-              class="form-input-label"
-              type="password"
-              id="fname"
-              name="password"
-              placeholder="PASSWORD"
-              style="outline: none; border: none"
-              v-model="userpw"
-            />
-          </div>
-  
-          <button class="login-btn" @click="handleSubmit">LOGIN</button>
-          <br />
-          <p>{{ Error }}</p>
-          <br />
-          <a class="forgotpassword" href="#">HAVE YOU FORGOTTEN YOUR PASSWORD?</a>
+<template>
+  <div>
+    <div class="container-login" style="margin-top: 2vh;">
+      <div class="Left-login-form" style="margin-top: 40vh;">
+        <h3 class="login-heading">LOG IN TO YOUR ACCOUNT</h3>
+        <div class="form-input-label">
+          <input
+            class="form-input-label"
+            type="text"
+            id="fname"
+            name="email"
+            placeholder="E-MAIL"
+            v-model="useremail"
+          />
+          <input
+            class="form-input-label"
+            type="password"
+            id="fname"
+            name="password"
+            placeholder="PASSWORD"
+            style="outline: none; border: none"
+            v-model="userpw"
+          />
         </div>
-  
-        <div class="right-login-form" style="margin-top: 40vh;">
-          <h4>NEED ACCOUNT</h4>
-          <router-link to="/signup" class="create-btn" type="button">REGISTER</router-link>
-        </div>
+
+        <button class="login-btn" @click="handleSubmit">LOGIN</button>
+        <br />
+        <p>{{ Error }}</p>
+        <br />
+        <a class="forgotpassword" href="#">HAVE YOU FORGOTTEN YOUR PASSWORD?</a>
+      </div>
+
+      <div class="right-login-form" style="margin-top: 40vh;">
+        <h4>NEED ACCOUNT</h4>
+        <router-link to="/signup" class="create-btn" type="button">REGISTER</router-link>
       </div>
     </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, ref } from 'vue';
-  import axios from 'axios';
-  
-  interface User {
-    useremail: string;
-    userpw: string;
-  }
-  
-  export default defineComponent({
-    name: 'LoginForm',
-    data() {
-      return {
-        useremail: '',
-        userpw: '',
-        Error: '',
-      };
-    },
-    methods: {
-      async handleSubmit() {
-        try {
-          const user: User = {
-            useremail: this.useremail,
-            userpw: this.userpw,
-          };
-  
-          const res = await axios.post('http://localhost:5000/api/user/login', user, {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });
-  
-          window.localStorage.setItem('User', JSON.stringify(res.data));
-          this.Error = 'Authentication successful';
-          window.location.href = '/';
-        } catch (err) {
-          console.log(err);
-          this.Error = String(err);
-        }
-      },
-    },
-  });
-  </script>
-<style>
-    .container-login{
-        /* margin-top: 2vh;
-        margin-top: 1.6em; */
-        width: 60%;
-        display: flex;
-        color: gray;
-        font-size: 90%;
-        margin:14%;
-        justify-content: space-between;
-        /* margin: 9%; */
-        margin-top: 10%; 
-    }
-    .Left-login-form{
-        margin-top:40vh ;
-        /* width: 40%; */
-    }
-    .form-input-label{
-        outline: none;
-        border: none;
-        width: 80%;
-        padding:5px 12px 0 20px;
-        margin: 18px 0;
-        box-sizing: border-box;
-        border: none;
-        border-bottom: 1px solid gray;
-        font-size:100%;
-    }
-    .right-login-form{
-        width: 50%;
-        margin-top: 8%;
-        margin-top:40vh ;
-    }
-    .login-heading{
-        margin: 4%;
-        color: black;
-    }
-    .login-btn{
-        background-color: black;
-        width: 80%;
-        height: 13%;
-        text-align: center;
-        font-size: 90%;
-        color: white;
-        text-decoration: none;
-        margin-top: 6%;
-    }
-    .forgotpassword{
-        color: black;
-        font-size: 70%;
+  </div>
+</template>
 
-    }
-    .create-btn{
-        background-color: transparent;
-        border: 0.5px solid black;
-        text-align: center;
-        font-size: 90%;
-        color: black;
-        text-decoration: none;
-        padding:2% 32% 3% 10%;
-    }
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import axios from 'axios';
+
+interface User {
+  useremail: string;
+  userpw: string;
+}
+
+export default defineComponent({
+  name: 'LoginForm',
+  data() {
+    return {
+      useremail: '',
+      userpw: '',
+      Error: '',
+    };
+  },
+  methods: {
+    async handleSubmit() {
+      try {
+        const user: User = {
+          useremail: this.useremail,
+          userpw: this.userpw,
+        };
+
+        const res = await axios.post('http://localhost:5000/api/user/login', user, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        window.localStorage.setItem('User', JSON.stringify(res.data));
+        this.Error = 'Authentication successful';
+        window.location.href = '/';
+      } catch (err) {
+        console.log(err);
+        this.Error = String(err);
+      }
+    },
+  },
+});
+</script>
+
+<style>
+.container-login {
+  width: 60%;
+  display: flex;
+  color: gray;
+  font-size: 90%;
+  margin: 14%;
+  justify-content: space-between;
+  margin-top: 10%;
+}
+
+.Left-login-form {
+  margin-top: 40vh;
+}
+
+.form-input-label {
+  outline: none;
+  border: none;
+  width: 80%;
+  padding: 5px 12px 0 20px;
+  margin: 18px 0;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid gray;
+  font-size: 100%;
+  font-family: 'Helvetica Neue', Helvetica, Arial, Lucida Grande, sans-serif;
+}
+
+.right-login-form {
+  width: 50%;
+  margin-top: 8%;
+  margin-top: 40vh;
+}
+
+.login-heading {
+  margin: 4%;
+  color: black;
+  font-family: 'Helvetica Neue', Helvetica, Arial, Lucida Grande, sans-serif;
+}
+
+.login-btn {
+  background-color: black;
+  width: 80%;
+  height: 13%;
+  text-align: center;
+  font-size: 90%;
+  color: white;
+  text-decoration: none;
+  margin-top: 6%;
+  font-family: 'Helvetica Neue', Helvetica, Arial, Lucida Grande, sans-serif;
+}
+
+.forgotpassword {
+  color: black;
+  font-size: 70%;
+  font-family: 'Helvetica Neue', Helvetica, Arial, Lucida Grande, sans-serif;
+}
+
+.create-btn {
+  background-color: transparent;
+  border: 0.5px solid black;
+  text-align: center;
+  font-size: 90%;
+  color: black;
+  text-decoration: none;
+  padding: 2% 32% 3% 10%;
+  font-family: 'Helvetica Neue', Helvetica, Arial, Lucida Grande, sans-serif;
+}
 </style>
--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -1,81 +1,81 @@
 <template>
-    <div style="background-color: white">
-      <input
-        style="font-family: 'Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; color: rgb(134, 134, 134); font-size: 15px; margin-top: 200px"
-        class="searchbar"
-        type="text"
-        placeholder="Search for an item"
-        v-model="search"
-        @input="handleSearch"
-      />
-  
-      <div v-for="result in filteredItems" :key="result.productId">
-        <img :src="result.productimage" alt="image" style="width: 230px; height: 350px" />
-        <div>Product Name: {{ result.productname }}</div>
-        <div>Product Price: {{ result.productprice }}</div>
-      </div>
+  <div style="background-color: white">
+    <input
+      style="font-family: 'Helvetica Neue', Helvetica, Arial, Lucida Grande, sans-serif; color: rgb(134, 134, 134); font-size: 15px; margin-top: 200px"
+      class="searchbar"
+      type="text"
+      placeholder="Search for an item"
+      v-model="search"
+      @input="handleSearch"
+    />
+
+    <div v-for="result in filteredItems" :key="result.productId">
+      <img :src="result.productimage" alt="image" style="width: 230px; height: 350px" />
+      <div>Product Name: {{ result.productname }}</div>
+      <div>Product Price: {{ result.productprice }}</div>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        search: '',
-        results: [],
-        filteredItems: [],
-      };
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      search: '',
+      results: [],
+      filteredItems: [],
+    };
+  },
+  methods: {
+    handleSearch() {
+      const filtered = this.results.filter((item) =>
+        item.productname.toLowerCase().includes(this.search.toLowerCase())
+      );
+      this.filteredItems = filtered;
+      console.log(this.filteredItems, 'this is data');
     },
-    methods: {
-      handleSearch() {
-        const filtered = this.results.filter((item) =>
-          item.productname.toLowerCase().includes(this.search.toLowerCase())
-        );
-        this.filteredItems = filtered;
-        console.log(this.filteredItems, 'this is data');
-      },
-      fetchData(searchTerm) {
-        axios
-          .get(`http://localhost:5000/api/products/`)
-          .then((response) => {
-            this.results = response.data;
-            console.log(response.data);
-            console.log('hi');
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      },
+    fetchData(searchTerm) {
+      axios
+        .get(`http://localhost:5000/api/products/`)
+        .then((response) => {
+          this.results = response.data;
+          console.log(response.data);
+          console.log('hi');
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
-    watch: {
-      search: {
-        handler(value) {
-          clearTimeout(this.searchTimer);
-          this.searchTimer = setTimeout(() => {
-            this.fetchData(value);
-          }, 300);
-        },
-        immediate: true,
+  },
+  watch: {
+    search: {
+      handler(value) {
+        clearTimeout(this.searchTimer);
+        this.searchTimer = setTimeout(() => {
+          this.fetchData(value);
+        }, 300);
       },
+      immediate: true,
     },
-  };
-  </script>
-  
-  <style scoped>
-  .searchbar {
+  },
+};
+</script>
+
+<style scoped>
+.searchbar {
   margin-top: 1.6em;
   width: 98%;
   outline: none;
   border: none;
   border-bottom: 1px solid rgb(8, 8, 8);
   padding: 0.2em 0em;
+  font-family: 'Helvetica Neue', Helvetica, Arial, Lucida Grande, sans-serif;
 }
 
 .searchbar::placeholder {
   color: rgb(30, 80, 95);
   margin-bottom: 20px;
 }
-  </style>
-  
+</style>
