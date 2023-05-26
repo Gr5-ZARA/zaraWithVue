468b91871daeb18f0526b87723873efe423e8661
Tarek-Issaoui <tarek9.9issaoui@gmail.com>
2023-05-26 16:58:32 +0100

    commit to pull

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,10 +2,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import {ContextProvider} from './views/Context.vue'
+
 
 const app = createApp(App)
 
 app.use(router)
 
-ContextProvider(app).mount('#app')
+app.mount('#app')
