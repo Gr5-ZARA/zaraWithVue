a422682e526c845d8827613a616013cb798d0716
^[[200~Islem-Az <isleem.azouuz@gmail.com>
2023-05-28 11:19:10 +0200

    commit to pull

--- client/src/views/Navbar.vue
+++ client/src/views/Navbar.vue
@@ -1,131 +1,131 @@
 <template >
-    <header style="width: 100%;padding: 12px 24px 20px 24px;position: fixed;position:fixed;"
+  <header style="width: 100%;padding: 12px 24px 20px 24px;position: fixed;position:fixed;"
+        
+      >
+    <div id="headerContents" >
+      <div id="headerLeft" style="display: flex ;">
+        <div>
+          <input
+            type="image"
+            name="menu"
+            src="https://icon-library.com/images/menu-icon-png-3-lines/menu-icon-png-3-lines-14.jpg"
+            id="menu"
+            style="width: 30px; height: 30px; margin-right: 30px ; "
+            @click="toggleMenu"
+          />
+        </div>
+        <RouterLink to="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/f/fd/Zara_Logo.svg"
+            alt="Zara_Logo"
+            id="logo"
+            style=" width: 212px"
+          />
+        </RouterLink>
+      </div>
+      <div id="headerRight">
+        <div id="headerRightLeft">
           
-        >
-      <div id="headerContents" >
-        <div id="headerLeft" style="display: flex ;">
-          <div>
-            <input
-              type="image"
-              name="menu"
-              src="https://icon-library.com/images/menu-icon-png-3-lines/menu-icon-png-3-lines-14.jpg"
-              id="menu"
-              style="width: 30px; height: 30px; margin-right: 30px ; "
-              @click="toggleMenu"
-            />
-          </div>
-          <RouterLink to="/">
+          <RouterLink
+            to="/search"
+            style="padding: 0% 60% 0% 0%; font-size: 13px; text-decoration: none; border-bottom: 1px solid black; color: rgb(41, 41, 41); font-weight: 700; font-family: Neue-Helvetica, Helvetica, Arial, Sans-Serif; font-stretch: ultra-condensed;"
+          >
+            <span class="menuLink">SEARCH</span>
+          </RouterLink>
+        </div>
+        <div id="headerRightRight">
+          <span class="menuLink" v-if="storedData" style=" font-size: 13px; color: rgb(41, 41, 41); font-weight: 100; font-family: Neue-Helvetica, Helvetica, Arial, Sans-Serif; font-stretch: ultra-condensed;" @click="logout">LOG OUT</span>
+          <RouterLink
+          to="/login" style="text-decoration: none; padding: 0px 10px 0px 10px; font-size: 13px; color: rgb(41, 41, 41); font-weight: 100; font-family: Neue-Helvetica, Helvetica, Arial, Sans-Serif; font-stretch: ultra-condensed;"
+          
+          v-else
+          >
+            <span class="menuLink" >LOG IN</span>
+          </RouterLink>
+
+          <RouterLink
+            to="/help"
+            style="text-decoration: none; padding: 0px 10px 0px 10px; font-size: 13px; color: rgb(41, 41, 41); font-weight: 100; font-family: Neue-Helvetica, Helvetica, Arial, Sans-Serif; font-stretch: ultra-condensed;"
+          >
+            <span class="menuLink">HELP</span>
+          </RouterLink>
+
+          <RouterLink
+            to="/cart"
+            style="text-decoration: none; padding: 0px 10px; font-size: 13px; color: rgb(41, 41, 41); font-weight: 100; font-family: Neue-Helvetica, Helvetica, Arial, Sans-Serif; font-stretch: ultra-condensed;"
+            v-if="parseData"
+            >
             <img
-              src="https://upload.wikimedia.org/wikipedia/commons/f/fd/Zara_Logo.svg"
-              alt="Zara_Logo"
-              id="logo"
-              style=" width: 212px"
+              style= "width: 22px; height: 19px;"
+              src="https://cdn-icons-png.flaticon.com/512/3110/3110065.png"
+              alt="Cart"
+              v-if="parseData.user[0].isAdmin===0"
             />
           </RouterLink>
         </div>
-        <div id="headerRight">
-          <div id="headerRightLeft">
-            
-            <RouterLink
-              to="/search"
-              style="padding: 0% 60% 0% 0%; font-size: 13px; text-decoration: none; border-bottom: 1px solid black; color: rgb(41, 41, 41); font-weight: 700; font-family: Neue-Helvetica, Helvetica, Arial, Sans-Serif; font-stretch: ultra-condensed;"
-            >
-              <span class="menuLink">SEARCH</span>
-            </RouterLink>
-          </div>
-          <div id="headerRightRight">
-            <span class="menuLink" v-if="storedData" style=" font-size: 13px; color: rgb(41, 41, 41); font-weight: 100; font-family: Neue-Helvetica, Helvetica, Arial, Sans-Serif; font-stretch: ultra-condensed;" @click="logout">LOG OUT</span>
-            <RouterLink
-            to="/login" style="text-decoration: none; padding: 0px 10px 0px 10px; font-size: 13px; color: rgb(41, 41, 41); font-weight: 100; font-family: Neue-Helvetica, Helvetica, Arial, Sans-Serif; font-stretch: ultra-condensed;"
-            
-            v-else
-            >
-              <span class="menuLink" >LOG IN</span>
-            </RouterLink>
-  
-            <RouterLink
-              to="/help"
-              style="text-decoration: none; padding: 0px 10px 0px 10px; font-size: 13px; color: rgb(41, 41, 41); font-weight: 100; font-family: Neue-Helvetica, Helvetica, Arial, Sans-Serif; font-stretch: ultra-condensed;"
-            >
-              <span class="menuLink">HELP</span>
-            </RouterLink>
-  
-            <RouterLink
-              to="/cart"
-              style="text-decoration: none; padding: 0px 10px; font-size: 13px; color: rgb(41, 41, 41); font-weight: 100; font-family: Neue-Helvetica, Helvetica, Arial, Sans-Serif; font-stretch: ultra-condensed;"
-              v-if="parseData"
-              >
-              <img
-                style= "width: 22px; height: 19px;"
-                src="https://cdn-icons-png.flaticon.com/512/3110/3110065.png"
-                alt="Cart"
-                v-if="parseData.user[0].isAdmin===0"
-              />
-            </RouterLink>
-          </div>
-        </div>
       </div>
-      
-      <div v-if="isMenuOpen" id="menuModal" class="menuOpacity animatedMenuShow">
-        <div class="menuContent">
-          <header id="headerRight" style="width: 1000%; display: flex; margin-right: 30px; padding: 10px; justify-content: space-between;">
-            <div id="menuTop"></div>
-          </header>
-          <div
-          id="menuOptions"
-          style="position: fixed; overflow: hidden; height: 4000px; width: 470px; background-color: white; margin-top: -30px; margin-left: -20px;"
-        >
-          <div id="menuMainOptions" style="display: flex;">
-            <ul class="menuOptionsList">
-              <ul class="close" @click="toggleMenu" style="margin-right: 100px; margin-left: -41px; font-size: 21px; color: grey;">
-                ✕
+    </div>
+    
+    <div v-if="isMenuOpen" id="menuModal" class="menuOpacity animatedMenuShow">
+      <div class="menuContent">
+        <header id="headerRight" style="width: 1000%; display: flex; margin-right: 30px; padding: 10px; justify-content: space-between;">
+          <div id="menuTop"></div>
+        </header>
+        <div
+        id="menuOptions"
+        style="position: fixed; overflow: hidden; height: 4000px; width: 470px; background-color: white; margin-top: -30px; margin-left: -20px;"
+      >
+        <div id="menuMainOptions" style="display: flex;">
+          <ul class="menuOptionsList">
+            <ul class="close" @click="toggleMenu" style="margin-right: 100px; margin-left: -41px; font-size: 21px; color: grey;">
+              ✕
+            </ul>
+            
+            <div style="margin-top: 90px; display: flex; flex-wrap: wrap; margin-bottom: 25px;">
+              <p class="menuOption" style="margin-left: -10px; margin-top: -40px; font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; color: grey; font-size: 11px; cursor: pointer;" @click="handleShow('woman')">
+                WOMAN
+              </p>
+              <p class="menuOption" style="margin-right: 20px; margin-top: -40px; margin-left: 25px; font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; color: rgb(134, 134, 134); font-size: 11px; cursor: pointer;" @click="handleShow('men')">
+                MEN
+              </p>
+              <p class="menuOption" style="margin-right: 20px; margin-top: -40px; font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; color: rgb(134, 134, 134); font-size: 11px; cursor: pointer;" @click="handleShow('kid')">
+                CHILDREN
+              </p>
+              <p class="menuOption" style="margin-right: 10px; margin-top: -40px; font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; color: rgb(134, 134, 134); font-size: 11px; cursor: pointer;" @click="handleShow('beauty')">
+                BEAUTY
+              </p>
+            </div>
+            <li class="menuOption" style="margin-top: -20px; font-size: 20px;">NEW</li>
+            
+            <div class="menuOption" v-if="display === 'men'">
+              <Men />
+            </div>
+            <div class="menuOption" v-if="display === 'woman'">
+              <Woman />
+            </div>
+            <div class="menuOption" v-if="display === 'kid'">
+              <Kid />
+            </div>
+            <div class="menuOption" v-if="display === 'beauty'" style="text-decoration: none; cursor: default;">
+              <Beauty />
+            </div>
+            <div id="menuExtraOptions">
+              <ul>
+                <li>JOIN LIFE</li>
+                 <li v-if="parseData"><RouterLink to="/addProduct" v-if="parseData.user[0].isAdmin>0">Add Product</RouterLink></li>
+                 <li v-if="parseData"><RouterLink to="/allProducts" v-if="parseData.user[0].isAdmin>0">Show All Products</RouterLink></li>
+                 <li v-if="parseData"><RouterLink to="/allUsers" v-if="parseData.user[0].isAdmin>0">Show All Users</RouterLink></li>
               </ul>
-              
-              <div style="margin-top: 90px; display: flex; flex-wrap: wrap; margin-bottom: 25px;">
-                <p class="menuOption" style="margin-left: -10px; margin-top: -40px; font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; color: grey; font-size: 11px; cursor: pointer;" @click="handleShow('woman')">
-                  WOMAN
-                </p>
-                <p class="menuOption" style="margin-right: 20px; margin-top: -40px; margin-left: 25px; font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; color: rgb(134, 134, 134); font-size: 11px; cursor: pointer;" @click="handleShow('men')">
-                  MEN
-                </p>
-                <p class="menuOption" style="margin-right: 20px; margin-top: -40px; font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; color: rgb(134, 134, 134); font-size: 11px; cursor: pointer;" @click="handleShow('kid')">
-                  CHILDREN
-                </p>
-                <p class="menuOption" style="margin-right: 10px; margin-top: -40px; font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; color: rgb(134, 134, 134); font-size: 11px; cursor: pointer;" @click="handleShow('beauty')">
-                  BEAUTY
-                </p>
-              </div>
-              <li class="menuOption" style="margin-top: -20px; font-size: 20px;">NEW</li>
-              
-              <div class="menuOption" v-if="display === 'men'">
-                <Men />
-              </div>
-              <div class="menuOption" v-if="display === 'woman'">
-                <Woman />
-              </div>
-              <div class="menuOption" v-if="display === 'kid'">
-                <Kid />
-              </div>
-              <div class="menuOption" v-if="display === 'beauty'" style="text-decoration: none; cursor: default;">
-                <Beauty />
-              </div>
-              <div id="menuExtraOptions">
-                <ul>
-                  <li>JOIN LIFE</li>
-                   <li v-if="parseData"><RouterLink to="/addProduct" v-if="parseData.user[0].isAdmin>0">Add Product</RouterLink></li>
-                   <li v-if="parseData"><RouterLink to="/allProducts" v-if="parseData.user[0].isAdmin>0">Show All Products</RouterLink></li>
-                   <li v-if="parseData"><RouterLink to="/allUsers" v-if="parseData.user[0].isAdmin>0">Show All Users</RouterLink></li>
-                </ul>
-              </div>
+            </div>
 
-          </ul>
-        </div>
-     
+        </ul>
       </div>
    
     </div>
-   
+ 
   </div>
+ 
+</div>
 </header>
 
 </template>
@@ -142,55 +142,55 @@ import HomeView from './HomeView.vue';
 export default defineComponent({
 name: 'Header',
 components: {
-  Woman,
-  Men,
-  Kid,
-  Beauty,
-  HomeView
-  
+Woman,
+Men,
+Kid,
+Beauty,
+HomeView
+
 },
-    setup() {
-      const router=useRouter()
-      const isMenuOpen = ref(false);
-      const display = ref('');
-      const storedData=window.localStorage.getItem('User')
-      var parseData;
-      if(storedData){
-        parseData=JSON.parse(storedData)
-      }
-      const toggleMenu = () => {
-        isMenuOpen.value = !isMenuOpen.value;
-      };
-      const logout=()=>{
-        console.log('clicked')
-        window.localStorage.removeItem('User')
-        window.location.replace('/')
-      }
-      const handleShow = (option:any  ) => {
-        display.value = option;
-      };
-      
-      return {
-        isMenuOpen,
-        display,
-        storedData,
-        parseData,
-        toggleMenu,
-        handleShow,
-        logout
-      };
+  setup() {
+    const router=useRouter()
+    const isMenuOpen = ref(false);
+    const display = ref('');
+    const storedData=window.localStorage.getItem('User')
+    var parseData;
+    if(storedData){
+      parseData=JSON.parse(storedData)
     }
+    const toggleMenu = () => {
+      isMenuOpen.value = !isMenuOpen.value;
+    };
+    const logout=()=>{
+      console.log('clicked')
+      window.localStorage.removeItem('User')
+      window.location.replace('/')
+    }
+    const handleShow = (option:any  ) => {
+      display.value = option;
+    };
+    
+    return {
+      isMenuOpen,
+      display,
+      storedData,
+      parseData,
+      toggleMenu,
+      handleShow,
+      logout
+    };
+  }
 });
 </script>
 
 <style scoped>
 header {
-  width: 100%;
-  padding: 12px 24px 20px 24px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 100;
+width: 100%;
+padding: 12px 24px 20px 24px;
+position: fixed;
+top: 0;
+left: 0;
+z-index: 100;
 }
 
 /* Header container */
@@ -329,9 +329,9 @@ padding: 0;
 margin-top: 10px;
 }
 .menuOption {
-  text-decoration: none !important;
-  cursor: default !important;
-   color: grey; 
-  font-size: x-small;
+text-decoration: none !important;
+cursor: default !important;
+ color: grey; 
+font-size: x-small;
 }
 </style>
